18ba1913f717b8a0f7f752fd48af08c228a28344
Mohamad Deyaa Akil <m.deyaa.akil@gmail.com>
2019-01-27 16:52:17 +0100

    WunschPizza JavaCode, ein Fehler tritt auf und weiss ich noch nicht woran er liegt

--- app/src/main/java/de/tu_clausthal/in/informatikwerkstatt/deinepizza/MainActivity3.java
+++ app/src/main/java/de/tu_clausthal/in/informatikwerkstatt/deinepizza/MainActivity3.java
@@ -3,9 +3,11 @@ package de.tu_clausthal.in.informatikwerkstatt.deinepizza;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity3 extends AppCompatActivity {
@@ -14,7 +16,10 @@ public class MainActivity3 extends AppCompatActivity {
     RadioGroup radiog;
     RadioButton grossR, familieR;
     CheckBox annanas, basilikum, brokkoli, champignon, chili, spinat, thunfisch, tomate, fetakaese, knoblauch, mais, oliven, morzarella, haenchen, gorgonzola, salami, schinken, redonion;
-    float preis = (float) 5.5;
+    float preis, preis1, preis2, preis3, preis4, preis5, preis6, preis7, preis8, preis9, preis10, preis11, preis12, preis13, preis14, preis15, preis16, preis17, preis18;
+    TextView euro;
+    Button wppreis;
+
 
 
     @Override
@@ -43,33 +48,33 @@ public class MainActivity3 extends AppCompatActivity {
         annanas = (CheckBox) findViewById(R.id. annanas);
         if (annanas.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis1 =  (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis1 = (float)0.5;
             }
         }
 
         basilikum = (CheckBox) findViewById(R.id.basilikum);
         if (basilikum.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis2 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis2 = (float) 0.5;
             }
         }
 
         brokkoli = (CheckBox) findViewById(R.id.brokkoli );
         if (brokkoli.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis3 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis3 = (float) 0.5;
             }
         }
 
@@ -77,44 +82,44 @@ public class MainActivity3 extends AppCompatActivity {
         champignon = (CheckBox) findViewById(R.id.champignon );
         if (champignon.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis4 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis4 = (float) 0.5;
             }
         }
 
         chili = (CheckBox) findViewById(R.id.chili );
         if (chili.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis5 = (float)0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis5 = (float) 0.5;
             }
         }
 
         spinat = (CheckBox) findViewById(R.id.spinat );
         if (spinat.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis6 = (float)0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis6 = (float) 0.5;
             }
         }
 
         thunfisch = (CheckBox) findViewById(R.id.thunfisch );
         if (thunfisch.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis7 = (float)0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis7 = (float) 0.5;
             }
         }
 
@@ -122,123 +127,132 @@ public class MainActivity3 extends AppCompatActivity {
         tomate = (CheckBox) findViewById(R.id.tomate );
         if (tomate.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis8 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis8 = (float) 0.5;
             }
         }
 
         fetakaese = (CheckBox) findViewById(R.id.fetakaese );
         if (fetakaese.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis9 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis9 = (float)0.5;
             }
         }
 
         knoblauch = (CheckBox) findViewById(R.id.knoblauch );
         if (knoblauch.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis10 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis10 = (float)0.5;
             }
         }
 
         mais = (CheckBox) findViewById(R.id.mais );
         if (mais.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis11 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis11 = (float)0.5;
             }
         }
 
         oliven = (CheckBox) findViewById(R.id.oliven );
         if (oliven.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis12 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis12 = (float) 0.5;
             }
         }
 
         morzarella = (CheckBox) findViewById(R.id.morzarella );
         if (morzarella.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis13 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis13 = (float)0.5;
             }
         }
 
         haenchen = (CheckBox) findViewById(R.id.haenchen );
         if (haenchen.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis14 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis14 = (float) 0.5;
             }
         }
 
         gorgonzola = (CheckBox) findViewById(R.id.gorgonzola );
         if (gorgonzola.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis15 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis15 = (float)0.5;
             }
         }
 
         salami = (CheckBox) findViewById(R.id.salami );
         if (salami.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis16 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis16 = (float) 0.5;
             }
         }
 
         schinken = (CheckBox) findViewById(R.id.schinken );
         if (schinken.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis17 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis17 = (float)0.5;
             }
         }
 
         redonion = (CheckBox) findViewById(R.id.redonion );
         if (redonion.isChecked()) {
             if (familie = true) {
-                preis = (float) (preis + 0.7) ;
+                preis18 = (float) 0.7 ;
 
             }
             if (gross = true) {
-                preis = (float) (preis + 0.5);
+                preis18 = (float) 0.5;
             }
         }
+
+        wppreis = (Button) findViewById(R.id.wppreis);
+        wppreis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                preis = preis1+ preis2+ preis3+ preis4+ preis5+ preis6+ preis7+ preis8+ preis9+ preis10+ preis11+ preis12+ preis13+ preis14+ preis15+ preis16+ preis17+ preis18;
+                euro.setText(String.valueOf(preis));
+            }
+        });
 
 
 
